e35c3cd6b35c8c2f4adc3b5b6d5ec7f559af78d7
Fredis Ernesto Ramírez Borges <fredisernesto@users.noreply.github.com>
2023-08-24 00:47:08 -0400

    Include Water sensor and Rele module
    Include Water sensor and Rele Module

---
+++ SteamController/SteamController.ino
@@ -0,0 +1,117 @@
+#include <WiFi.h>
+#include <DNSServer.h>
+#include "DHT.h"
+
+//Pins definition
+#define DHTTYPE        DHT11
+#define DHTPIN         2 
+#define SteamValvePIN  23
+#define WaterValvePIN  22
+#define StrovePIN      21
+#define WaterLevelPIN  33
+#define StroveLevelPIN 32
+
+//Setpoints
+#define TempSnow       0
+#define HumSnow        20
+
+//Status
+bool SteamValve;
+bool WaterValve;
+bool Strove;
+int WaterLevel;
+bool StroveLevel;
+float Temp;
+float Hum;
+
+//
+const byte DNS_PORT = 53;
+IPAddress apIP(8,8,4,4); // The default android DNS
+DNSServer dnsServer;
+WiFiServer server(80);
+DHT dht(DHTPIN, DHTTYPE);
+
+String responseHTML = ""
+  "<!DOCTYPE html><html><head><title>ControlVapor</title></head><body>"
+  "<h1>Hello World!</h1><p>Sistema de Control de Vapor "
+  "</p></body></html>";
+
+void setup() {
+  pinMode(StrovePIN,OUTPUT);
+  pinMode(WaterValvePIN,OUTPUT);
+  pinMode(SteamValvePIN,OUTPUT);
+  //pinMode(WaterLevelPIN,INPUT);
+  pinMode(StroveLevelPIN,INPUT); 
+  digitalWrite(StrovePIN,HIGH);
+  digitalWrite(WaterValvePIN,LOW);
+  digitalWrite(SteamValvePIN,LOW);
+  dht.begin();
+  Serial.begin(9600);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("Unidad_Controladora_Vapor");
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+
+  // if DNSServer is started with "*" for domain name, it will reply with
+  // provided IP to all DNS request
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  server.begin();
+}
+
+void loop() {
+  // To read sensors
+  Temp = dht.readTemperature();
+  Hum = dht.readHumidity();
+  WaterLevel=analogRead(WaterLevelPIN);
+  Serial.print(F("Humidity: "));
+  Serial.print(Hum);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(Temp);
+  Serial.println(F("°C "));
+  Serial.print(F("Water level "));
+  Serial.println(WaterLevel);
+  
+  digitalWrite(StrovePIN,HIGH);
+  digitalWrite(WaterValvePIN,HIGH);
+  digitalWrite(SteamValvePIN,HIGH);
+  delay(1000);
+  digitalWrite(StrovePIN,LOW);
+  digitalWrite(WaterValvePIN,LOW);
+  digitalWrite(SteamValvePIN,LOW);
+  delay(1000);
+  //Control cicles
+  if(Temp<TempSnow)//Strove Steamvalve on
+  if(Temp>TempSnow)//Strove Steamvalve off
+  if(StroveLevel == false)//Strove Level Alarm
+  if(WaterLevel == false)//Water Level Alarm
+
+  delay(1000);
+  
+  
+  //Servers
+  dnsServer.processNextRequest();
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            client.print(responseHTML);
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+    client.stop();
+  }
+}
